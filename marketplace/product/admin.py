367e55d9fe5b4961b6e35190ab23560ba3861088
ngenzir <ngenzir@gmail.com>
2021-03-26 08:36:10 +0200

    product - admin

--- marketplace/product/admin.py
+++ marketplace/product/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import (
+	Product, 
+	MyProducts, 
+	Thumbnail, 
+	ProductRating,
+	CuratedProducts
+	)
+
+
+class ThumbnailInline(admin.TabularInline):
+	extra = 1
+	model = Thumbnail
+
+
+class ProductAdmin(admin.ModelAdmin):
+	inlines = [ThumbnailInline]
+	list_display = ["__unicode__", "description", "price", "sale_price"]
+	search_fields = ["title", "description"]
+	list_filter = ["price", "sale_price"]
+	list_editable = ["sale_price"]
+	class Meta:
+		model = Product
+
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+admin.site.register(MyProducts)
+
+
+admin.site.register(Thumbnail)
+
+admin.site.register(ProductRating)
+
+admin.site.register(CuratedProducts)
