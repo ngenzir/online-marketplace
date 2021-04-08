92cae6c03aeb432f55e3a29da9c4de29b3abbb09
ngenzir <ngenzir@gmail.com>
2021-04-08 08:49:27 +0300

    template tags

--- marketplace/products/views.py
+++ marketplace/products/views.py
@@ -1,4 +1,33 @@
-from django.shortcuts import render
+import os
+
+
+from django.shortcuts import render, get_object_or_404
+from django.conf import settings
+from django.core.servers.basehttp import FileWrapper
+from django.core.urlresolvers import reverse
+from django.db.models import Q, Avg, Count
+from django.http import Http404, HttpResponse, JsonResponse
+from django.views.generic import View
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.list import ListView
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def home(request):
 	return render("products/home.html")
