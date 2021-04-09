431bbd056799172b07e5424be3f2b6ae7548891c
ngenzir <ngenzir@gmail.com>
2021-04-09 18:18:01 +0300

    create new thumbnail

--- marketplace/products/models.py
+++ marketplace/products/models.py
@@ -1,10 +1,15 @@
 
+import os
+import shutil
+from PIL import Image
+import random
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from django.urls import reverse
 from django.utils.text import slugify
+from django.core.files import File
 
 
 
@@ -88,7 +93,7 @@ THUMB_CHOICES = (
 
 
 class Thumbnail(models.Model):
-	product = modles.ForeignKey(Product)
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
 	type = models.CharField(max_length=20, choices=THUMB_CHOICES, default='hd')
 	height = models.CharField(max_length=20, null=True, blank=True)
 	width = models.CharField(max_length=20, null=True, blank=True)
@@ -102,4 +107,34 @@ class Thumbnail(models.Model):
 	def __unicode__(self):
 		return str(self.media.path)
 
-	
+
+def create_new_thumbnail (media_path, instance, owner_slug):
+	filename = os.path.basename(media_path)
+	thumb = Image.open(media_path)
+	size = (max_length, max_width)
+	thumb.thumbnail(size, Image.ANTIALIAS)
+	temp_loc = "%s/%s/tmp" %(settings.MEDIA_ROOT, owner_slug)
+	if not os.path.exists(temp_loc):
+		os.makedirs(temp_loc)
+	temp_file_path = os.path.join(temp_loc, filename)
+	if os.path.exists(temp_file_path):
+		temp_path = os.path.join(temp_loc, "%s" %(random.random()))
+		os.temp_file_path = os.path.join(temp_path, filename)
+	temp_image = open(temp_file_path, "w")
+	thumb.save(temp_image)
+	thumb_data =open(temp_file_path, "r")
+
+	thumb_file = File(thumb_data)
+	instance.media.save(filename, thumb_file)
+	shutil.rmtree(temp_loc, ignore_errors=True)
+	return True
+
+
+
+
+
+
+
+
+
+
